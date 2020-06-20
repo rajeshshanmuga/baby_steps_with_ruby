af0ddfc2e0c5e8cd52ff2365a0bcf3755f60061f
rajeshshanmugam <rajeshshanmugam@positivenaick.com>
2020-06-21 01:01:42 +0530

    encapsulation expriment

--- oops_1.rb
+++ oops_1.rb
@@ -13,16 +13,28 @@ class Custoemr
     attr_accessor :age
     attr_accessor :id
     def show_cuurent_cutomer
+        @encapsule_var_1 = "I'm not the direct relative" # For expirementing encapsulation
         puts @name, @age, @id
     end 
     
     def total_customers
+        '''
+        Needs to have the account of how many customers are inside the system but without 
+        initialization(self) method, It is not effective.
+        '''
         @@custoemr_count = @@custoemr_count +1
         puts @@custoemr_count
     end
     
     def psudo_attr_reader
-        @psudo_variable = 'Workign'
+        @psudo_variable = 'Workign' # For expiremnting attr_reader
+    end
+    def demo_encapsulate
+        '''
+        It doent what is the variable it is going to provide or how it is formed, It only delivers
+        Used for expirementing encapsulation
+        '''
+        puts @encapsule_var_1
     end
 end 
 
@@ -35,3 +47,4 @@ vadikaiyalar.total_customers
 vadikaiyalar.total_customers
 puts vadikaiyalar.psudo_attr_reader
 puts vadikaiyalar.psudo_variable
+vadikaiyalar.demo_encapsulate
